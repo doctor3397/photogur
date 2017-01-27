cbdbdcf269b425c99d56f8004e2a827a16dc74c5
doctor3397 <hs2546@columbia.edu>
2017-01-27 15:05:50 -0500

    Create a 'picture/:id'=>'pciture#show' route, create the show method and create the show.html.erb file

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   # match any HTTP GET Request for the URL /pictures to the index action in the PicturesController"
   get 'pictures' => 'pictures#index'
-
+  get 'pictures/:id' => 'pictures#show', as: 'picture'
+  # as: "picture", refer to this route as picture_path: the String is prepended to _path
 end
