e59c22fb28678df01e8893a04e312435060f5b1b
doctor3397 <hs2546@columbia.edu>
2017-01-27 18:47:04 -0500

    Add resources :pictures in route, modify the Delete method in show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   root 'pictures#index'
 
-  get 'pictures/new' => 'pictures#new'
-  post 'pictures' => 'pictures#create'
-
+  resources :pictures
   # match any HTTP GET Request for the URL /pictures to the index action in the PicturesController"
-  get 'pictures' => 'pictures#index'
-  get 'pictures/:id' => 'pictures#show', as: 'picture'
-  # as: "picture", refer to this route as picture_path: the String is prepended to _path
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-  patch 'pictures/:id' => "pictures#update"
-
-  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+  # get 'pictures' => 'pictures#index'
+  # get 'pictures/new' => 'pictures#new'
+  #
+  # get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  # post 'pictures' => 'pictures#create'
+  #
+  # get 'pictures/:id' => 'pictures#show', as: 'picture'
+  # # as: "picture", refer to this route as picture_path: the String is prepended to _path
+  # patch 'pictures/:id' => "pictures#update"
+  #
+  # delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
 end
