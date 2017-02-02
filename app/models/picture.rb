b0e2dd9ae09bea69150b9762d33a5ca5d0678bd6
doctor3397 <hs2546@columbia.edu>
2017-02-02 17:52:08 -0500

    Changing db to pg in production

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,10 @@
 class Picture < ApplicationRecord
+  #validates before the data is saved in db
+  validates :artist, presence: true
+  # minimum length 3 characters, maximum length 20 characters
+  validates :title, length: { in: 3..20 }
+  #url: be present, be unique
+  validates :url, presence: true, uniqueness: true
+  validates_format_of :url, :with =>
+  /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/\S*)?/i
 end
