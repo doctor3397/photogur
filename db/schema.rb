e147d76a2692467d31b897e7d42290c51ffb2edb
doctor3397 <hs2546@columbia.edu>
2017-02-06 10:45:21 -0500

    Add user model with bin/rails g model User email:string password_digest:string --no-test-framework

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203162823) do
+ActiveRecord::Schema.define(version: 20170206154245) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20170203162823) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
